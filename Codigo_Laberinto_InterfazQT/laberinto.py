cf4d63a0118c24c8067f1bd0738ff2b0b36ae432
Carlit3r <carlosa.lopezb@uqvirtual.edu.co>
2023-04-25 18:16:13 -0500

    Coloqué la detección de color y me declaré gay

--- Codigo_Laberinto_InterfazQT/laberinto.py
+++ Codigo_Laberinto_InterfazQT/laberinto.py
@@ -1,19 +1,29 @@
-#importar dependencias necesarias para ejecutar qt
-import sys, json # libreria que habilita funcionaliades del sistema
+# Dependencias necesarias de  QT
+import sys, json # Libreria que habilita funcionaliades del sistema
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import  uic
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import QTimer, Qt
 from PyQt5.QtGui import QImage, QPixmap
-#modulos asociados al serial
+
+# Módulos asociados al serial
 import serial #dwddl
 import serial.tools.list_ports as list_ports
 from time import sleep
 
+# Módulos asociados al seguimiento de objetos: 
+from collections import deque
+import numpy as np
 import cv2
 
-#otra prueba
-#leer datos disponibles en el puerto, cuando los haya se genera una señal, llamda update
+
+# Define los parámetros de color que detectará
+greenLower = (0, 192, 0)
+greenUpper = (255, 255, 195)
+pts = deque(maxlen=64)
+
+
+# Leer datos disponibles en el puerto, cuando los haya se genera una señal, llamda update
 class ReadPort(QtCore.QObject):
     update = QtCore.pyqtSignal(str)
     #constructor
@@ -47,18 +57,59 @@ class camaraInterfaz(QtCore.QObject):#QtCore.QObject
         self.activarCamara = True       
         #self.show()
         
-#Metodo para actualizar la imagen de la camara
+# Metodo para actualizar la imagen de la camara
     def updateCamera(self):
         while True:
             if self.activarCamara == True:
                 if self.cam.isOpened():
                     ret, frame = self.cam.read()
                     magenHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                    mask = cv2.inRange(magenHSV, greenLower, greenUpper)  # Recibe los parámetros de color
+                    mask = cv2.erode(mask, None, iterations=2)
+                    mask = cv2.dilate(mask, None, iterations=2)
 
-                    if not ret:#si no se tomo captura de imagen, retorne
+# - - - - - - - - - - Encontrar el contorno de la bola  y fijar el centro (x, y) - - - - - - - - - #
+                    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                        cv2.CHAIN_APPROX_SIMPLE)[-2]
+                    center = None
+                    
+                    # Sólo procede si encuentra al menos un contorno
+                    if len(cnts) > 0:
+                        # Encuentra el contorno más largo en mask
+                        
+                        # Luego lo usa y computa el mínimo circulito y centro
+                        c = max(cnts, key=cv2.contourArea)
+                        ((x, y), radius) = cv2.minEnclosingCircle(c)
+                        M = cv2.moments(c)
+                        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+                        # Sólo procede si el radio conoce un tamaño mínimo
+                        if radius > 10:
+                            cv2.circle(frame, (int(x), int(y)), int(radius), # Dibuja el círculo
+                                (0, 255, 255), 2)
+                            cv2.circle(frame, center, 5, (0, 0, 255), -1) # Actualiza puntos trazados
+                        
+                    # Actualiza lista de puntos
+                    pts.appendleft(center)
+
+                    # bucle sobre el conjunto de puntos rastreados
+                    for i in range(1, len(pts)):
+                        # Si cualquier de los puntos rastreados no es del color...
+                        # ignórelo.
+                        if pts[i - 1] is None or pts[i] is None:
+                            continue
+
+                        # Computa el grosor de la línea y...
+                        # dibuja los puntos conectados
+                        thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
+                        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+                        
+# - - - - - - - - - - - - - - - - - - VOLVEMOS A LA CÁMARA - -  - - - - - - - - - - - - - - - - - - #
+
+                    if not ret: #Si no se tomo captura de imagen, retorne
                         return
-                #convierte la imagen capturada con openCV a Qt
-                #imagen, anccho, alto, ancho*numerobits por pixel, formato de imagen
+                # Convierte la imagen capturada con openCV a Qt
+                # Imagen, anccho, alto, ancho*numerobits por pixel, formato de imagen
                 image = QImage(frame, frame.shape[1], frame.shape[0], frame.shape[1]*frame.shape[2], QImage.Format_RGB888)
                 
                 self.pixmap.convertFromImage(image.rgbSwapped())
@@ -76,7 +127,7 @@ class camaraInterfaz(QtCore.QObject):#QtCore.QObject
             
             
                 
-#creacion de la CLASE PRINCIPAL ----------------------------------
+# ------------------------------ Creación de la CLASE PRINCIPAL ---------------------------------- #
 class laberinto(QMainWindow):#herencia
     def __init__(self):
         super().__init__()#(laberinto, self).__init__() inputEdit
